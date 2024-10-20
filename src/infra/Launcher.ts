2345c81e9308a79c4171a88c98b500f79e92a3bc
Peter Siebold <peter@ilove.codes>
2024-10-20 19:55:44 +0200

    Added API Gateway to access Lamdba from outside

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,8 +1,12 @@
 import { App } from "aws-cdk-lib";
 import { DataStack } from "./stacks/DataStack";
 import { LamdbaStack } from "./stacks/LambdaStack";
+import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
 
 new DataStack(app, "DataStack");
-new LamdbaStack(app, "LambdaStack");
+const lambdaStack = new LamdbaStack(app, "LambdaStack");
+new ApiStack(app, "ApiStack", {
+    helloLambdaIntegration: lambdaStack.helloLamdbaIntegration
+})
--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -2,15 +2,20 @@ import {Stack, StackProps} from "aws-cdk-lib"
 import { Construct } from "constructs";
 import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "node:path";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 
 export class LamdbaStack extends Stack{
+    public readonly helloLamdbaIntegration: LambdaIntegration;
+
     constructor(scope: Construct, id: string, props?:StackProps){
         super(scope, id, props);
 
-        new LambdaFunction(this, "HelloLambda", {
+        const helloLambda = new LambdaFunction(this, "HelloLambda", {
             runtime: Runtime.NODEJS_18_X,
             handler: "hello.main",
-            code: Code.fromAsset(join(__dirname, "..", "services", "hello"))
+            code: Code.fromAsset(join(__dirname, "..", "..", "services"))
         })
+
+        this.helloLamdbaIntegration = new LambdaIntegration(helloLambda)
     }
 }
