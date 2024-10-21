76ebd78c5fa40ec8a4ac891d3e05abb1d77246c8
Peter Siebold <peter@ilove.codes>
2024-10-21 20:33:18 +0200

    Switched lamdba to using typescript

--- src/infra/Utils.ts
+++ src/infra/Utils.ts
@@ -1,7 +1,7 @@
 import { Fn, Stack } from "aws-cdk-lib";
 
 export function getSuffixFromStack(stack: Stack){
-    const shortStackId = Fn.select(2, Fn.split("/", this.stackId))
+    const shortStackId = Fn.select(2, Fn.split("/", stack.stackId))
     const stackSuffix = Fn.select(4, Fn.split("-", shortStackId))
 
     return stackSuffix
--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,9 +1,10 @@
 import {Stack, StackProps} from "aws-cdk-lib"
 import { Construct } from "constructs";
-import { Code, Function as LambdaFunction, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Code,  Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "node:path";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 
 interface LambdaStackProps extends StackProps{
     spacesTable: ITable
@@ -15,10 +16,11 @@ export class LamdbaStack extends Stack{
     constructor(scope: Construct, id: string, props:LambdaStackProps){
         super(scope, id, props);
 
-        const helloLambda = new LambdaFunction(this, "HelloLambda", {
+        const helloLambda = new NodejsFunction(this, "HelloLambda", {
             runtime: Runtime.NODEJS_18_X,
-            handler: "hello.main",
-            code: Code.fromAsset(join(__dirname, "..", "..", "services")),
+            handler: "handler",
+            entry: join(__dirname, "..", "..", "services", "hello.ts"),
+            // code: Code.fromAsset(join(__dirname, "..", "..", "services")),
             environment: {
                 TABLE_NAME: props.spacesTable.tableName
             }
