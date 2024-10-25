4e228291c5aac6709d0f49ec129d286a8ec56ebb
Peter Siebold <peter@ilove.codes>
2024-10-25 15:20:08 +0200

    Added basic required fields validation

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -4,6 +4,7 @@ import { postSpaces } from "./PostSpaces";
 import { getSpaces } from "./GetSpaces";
 import { updateSpace } from "./UpdateSpace";
 import { deleteSpace } from "./DeleteSpace";
+import { MissingFieldError } from "../shared/Validator";
 
 const ddbClient = new DynamoDBClient({})
 
@@ -30,6 +31,12 @@ async function handler(event: APIGatewayProxyEvent, context: Context): Promise<A
                 break;
         }
     } catch (error) {
+        if (error instanceof MissingFieldError) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify(error.message)
+            }
+        }
         return {
             statusCode: 500,
             body: JSON.stringify(error.message)
